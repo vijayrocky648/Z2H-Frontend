067d70b6e6f9be23abfa79ed34fe5c9d06e7fba7
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2024-06-06 22:18:48 +0530

    Added dist folder

---
+++ dist/spa/assets/globalVariable.67792417.js
@@ -0,0 +1,1 @@
+var l=async({app:a})=>{a.config.globalProperties.$tooltipDelay=750};export{l as default};
---
+++ dist/spa/assets/use-quasar.088138f3.js
@@ -0,0 +1,1 @@
+import{i as a,ab as r}from"./index.741a3cbc.js";function u(){return a(r)}export{u};
